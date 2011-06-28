7ccceeb55e4e2b7d1ef98025177a4dc2bb91aeab
ChameleonGeek <chameleongeek@gmail.com>
2011-06-27 21:18:00 -0500

    Agree the help command

---
+++ WorldServer/Console/Console.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shared;
+
+namespace CharacterServer
+{
+    [ConsoleHandler("help", 1, "<command>")]
+    public class ConsoleHelp : IConsoleHandler
+    {
+        public bool HandleCommand(string command, List<string> args)
+        {
+
+
+            switch (args[0])
+            {
+                case "create":
+                    Log.Success("Command Create", "Usage: .create : <username,Password> Example: .create qwerty 12345");
+                    break;
+
+                case "realm":
+                    Log.Success("Command Realm", "Usage: .realm : <id(<21),type(16-48-80),lang(0-1-3-5)> Example: .realm 33 48 3");
+                    break;
+
+                case "help":
+                    Log.Success("Command Help", "Usage: .help : <command> Example: .help realm");
+                    break;
+
+                default:
+                    Log.Notice("Command Unknown", "You did something wrong");
+                    break;
+
+            }
+
+            return true;
+        }
+
+    }
+}
